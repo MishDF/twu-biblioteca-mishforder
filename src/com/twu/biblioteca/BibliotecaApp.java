60c184f17c2a2b7fcf300e5ad7f39455e19335bb
Mish Forder <mishd14@gmail.com>
2019-04-05 03:07:06 +1100

    added story 1.6

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,64 @@
 package com.twu.biblioteca;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
+    static String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        System.out.println(welcomeMessage);
+
+        int menuInput = 0;
+
+        do {
+            showMenu();
+            try{
+                menuInput = scan.nextInt();
+                menuSelection(menuInput);
+
+            } catch(InputMismatchException e){
+                System.out.println("Invalid input");
+                scan.nextLine();
+            }
+
+
+        } while (menuInput!=3);
+
     }
+    public static void showMenu(){
+        //show options
+        System.out.println("Type a number to select an option");
+        System.out.println("1 - Show list of books");
+        System.out.println("3 - Quit");
+
+
+    }
+
+    public static void menuSelection(int i){
+        //switch to trigger right function
+        switch(i){
+            case 1 :
+                showBooks();
+                break;
+            case 3:
+                System.out.println("Goodbye");
+                break;
+            default:
+                System.out.println("Invalid number");
+                break;
+        }
+    }
+
+    public static void showBooks(){
+        //blank for now
+        System.out.println("running showing books");
+    }
+
+
+
+
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,4 +1,30 @@
 package com.twu.biblioteca;
 
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.InputMismatchException;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 public class BibliotecaAppTest {
+
+    @Test
+    public void welcomeMessageShown() {
+        TestCase.assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", BibliotecaApp.welcomeMessage);
+    }
+
+    @Test
+    public void menuSelectionTest(){
+        BibliotecaApp mockApp = Mockito.mock(BibliotecaApp.class);
+        //if 1 then will call book list
+        mockApp.menuSelection(1);
+        Mockito.verify(mockApp, Mockito.times(1)).showBooks();
+
+    }
+
+
+
 }
